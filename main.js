4ac3e3a6000dbfe6fa627b1dee6e27dac877f6ce
tikmargarian <redwolf2307@gmail.com>
2025-02-03 22:50:02 +0100

    initial commit

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function openNavbar () {
 
 hamburger.addEventListener("click", openNavbar);
 
-document.querySelectorAll(".navbar-items ul li a").forEach(item => 
+document.querySelectorAll(".navbar-items li a").forEach(item => 
     item.addEventListener("click", function () {
     hamburger.classList.remove("active");
     navbarItems.classList.remove("active");
@@ -35,3 +35,34 @@ avatars[3].style.backgroundColor = "#044944";
 avatars[4].style.backgroundColor = "#8bc24a";
 avatars[5].style.backgroundColor = "#254bdd";
 
+document.addEventListener('DOMContentLoaded', () => {
+    const contactForm = document.getElementById('contactForm');
+
+    emailjs.init('aNY0-EMR99N3Lj_BE');
+
+    contactForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+
+    
+        const formData = {
+            depart: form.depart.value, 
+            destination: form.destination.value,
+            name: form.name.value,
+            phone: form.phone.value,
+            message: form.description.value,
+        };
+
+        emailjs
+            .send('service_g3kgo4c', 'template_ocpvvym', formData)
+            .then(() => {
+                alert('Votre message a été envoyé avec succès !');
+                form.reset();
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                alert('Une erreur est survenue. Veuillez réessayer plus tard.');
+            });
+    });
+});
